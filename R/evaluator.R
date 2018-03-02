2a11d219e88a37723e1b5befc0a927c136c7a740
igjit <igjit1@gmail.com>
2018-03-02 21:02:22 +0900

    Implement evaluator

---
+++ R/evaluator.R
@@ -0,0 +1,56 @@
+#' Evaluate s-expression
+#'
+#' @param s s-expression
+#' @param ... additional arguments
+#' @export
+evaluate <- function(s, ...) {
+    base::eval(translate(s), ...)
+}
+
+#' Translate s-expression to R
+#'
+#' @param s s-expression
+#' @export
+translate <- function(s) {
+    compile(parse(s))
+}
+
+r_functions <- list(
+    "begin" = `{`,
+    "set!" = `<-`,
+    "define" = `<-`,
+    "=" = `==`,
+    "eq?" = `==`,
+    "equal?" = identical,
+    "not" = `!`,
+    "cons" = function(x, y) append(list(x), y),
+    "car" = function(x) x[[1]],
+    "cdr" = function(x) x[-1],
+    "list?" = is.list,
+    "null?" = function(x) identical(x, list()),
+    "symbol?" = is.name
+)
+
+compile <- function(x) {
+    if (is.character(x)) {              # variable reference
+        as.name(x)
+    } else if (!is.list(x)) {           # constant literal
+        x
+    } else if (length(x) == 0) {        # empty list
+        x
+    } else if (identical(x[[1]], "lambda")) { # (lambda (var*) exp*)
+        vars <- x[[2]]
+        exps <- x[-c(1, 2)]
+        args <- vector("list", length(vars))
+        names(args) <- as.character(vars)
+        body <- as.call(c(as.name("{"), lapply(exps, compile)))
+        call("function", as.pairlist(args), body)
+    } else {                            # other functions
+        r_func <- if (is.list(x[[1]])) NULL else r_functions[[x[[1]]]]
+        if (is.null(r_func)) {
+            as.call(lapply(x, compile))
+        } else {
+            as.call(c(r_func, lapply(x[-1], compile)))
+        }
+    }
+}
---
+++ tests/testthat/test-evaluator.R
@@ -0,0 +1,70 @@
+context("test-evaluator.R")
+
+test_that("arithmetic operations", {
+    expect_equal(evaluate("(+ 1 2)"), 3)
+    expect_equal(evaluate("(* 2 3)"), 6)
+    expect_equal(evaluate("(- 10 3)"), 7)
+    expect_equal(evaluate("(/ 100 4)"), 25)
+})
+
+test_that("list processing", {
+    expect_equal(evaluate("(list)"), list())
+    expect_equal(evaluate("(list 1 2)"), list(1, 2))
+    expect_equal(evaluate("(car (list 10 20))"), 10)
+    expect_equal(evaluate("(cdr (list 10 20 30))"), list(20, 30))
+    expect_equal(evaluate("(cons 1 (cons 2 (quote ())))"), list(1, 2))
+    expect_equal(evaluate("(cons (list 1 2) (quote ()))"), list(list(1, 2)))
+})
+
+test_that("if", {
+    expect_equal(evaluate("(if T 2 3)"), 2)
+    expect_equal(evaluate("(if F 2 3)"), 3)
+})
+
+test_that("quote", {
+    expect_equal(evaluate("(quote abc)"), as.name("abc"))
+    expect_equal(evaluate("(quote ())"), list())
+})
+
+test_that("begin", {
+    expect_equal(evaluate("(begin 1)"), 1)
+    expect_equal(evaluate("(begin 1 2 3)"), 3)
+})
+
+test_that("lambda", {
+    expect_equal(evaluate("((lambda (a b) (+ a b)) 1 2)"), 3)
+    expect_equal(evaluate("((lambda (a) (+ a 1)) 1)"), 2)
+    expect_equal(evaluate("((lambda (l) (cdr l)) (list 10 20))"), list(20))
+    expect_equal(evaluate("((lambda () (+ 1 2)))"), 3)
+})
+
+test_that("symbol?", {
+    expect_equal(evaluate("(symbol? (quote x))"), TRUE)
+    expect_equal(evaluate("(symbol? 1)"), FALSE)
+})
+
+test_that("equal?", {
+    expect_equal(evaluate("(equal? (list 1 (list 2)) (list 1 (list 2)))"), TRUE)
+})
+
+test_that("set!", {
+    env <- new.env()
+    evaluate("(set! a 1)", env)
+    expect_equal(env$a, 1)
+    evaluate("(set! a 2)", env)
+    expect_equal(env$a, 2)
+    evaluate("(set! a (+ 1 2))", env)
+    expect_equal(env$a, 3)
+})
+
+test_that("define", {
+    expect_equal(evaluate("(begin (define add2 (lambda (x) (+ x 2))) (add2 40))"), 42)
+    expect_equal(evaluate("(begin (define add3 (lambda (x) (set! x (+ x 1)) (set! x (+ x 2)) x)) (add3 4))"), 7)
+    expect_equal(evaluate("(begin (define a 1) (set! a 100) a)"), 100)
+    expect_equal(evaluate("(begin (define a 1) (set! a (list 2 3)) a)"), list(2, 3))
+    expect_equal(evaluate("(begin (define l (list 2 3 (list 4 5))) l)"), list(2, 3, list(4, 5)))
+
+    env <- new.env()
+    evaluate("(define map (lambda (f l) (if (null? l) (quote ()) (cons (f (car l)) (map f (cdr l))))))", env)
+    expect_equal(evaluate("(map (lambda (x) (+ x 10)) (list 1 2 3))", env), list(11, 12, 13))
+})

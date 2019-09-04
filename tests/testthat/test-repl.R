d14475a996292471b8298243b10fd66021c06e7f
igjit <igjit1@gmail.com>
2019-09-04 12:55:08 +0900

    Test repl

---
+++ tests/testthat/test-repl.R
@@ -0,0 +1,10 @@
+test_that("repl", {
+  readline_mock <- function(lines) {
+    i <- 0
+    function(prompt) lines[i <<- i + 1]
+  }
+  expect_output(
+    with_mock(readline = readline_mock(c("(+ 1 2)", "q")),
+              repl()),
+    paste("3", "bye.", sep = "\n"))
+})

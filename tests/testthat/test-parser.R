c90d77f3d82d56a7a79713a2a49b7a3efde15e7e
igjit <igjit1@gmail.com>
2019-09-07 21:23:57 +0900

    Check errors

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -4,4 +4,7 @@ test_that("parse", {
   expect_equal(parse("(1 2)"), list(1, 2))
   expect_equal(parse("(1 (2))"), list(1, list(2)))
   expect_equal(parse("()"), list())
+
+  expect_error(parse("("), "unexpected EOF while reading")
+  expect_error(parse(")"), "unexpected )")
 })
